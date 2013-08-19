1766767c10fab24fd5430f6acdc18c221a632df6
bmcorser <benmarshallcorser@gmail.com>
2013-08-19 03:24:14 +0100

    Register with Markdown hooks

--- pelicanfly/__init__.py
+++ pelicanfly/__init__.py
@@ -1,11 +1,15 @@
 from pelican import signals
+from fontawesome_markdown import FontAwesomeExtension
 
-def pelicanfly(pelican_instance):
-    pass
 
-def gen_init(generator_instance):
-    pass
+def pelicanfly(peli):
+    md_ext = peli.settings.get('MD_EXTENSIONS')
+    if md_ext:
+        if 'fontawesome_markdown' not in md_ext:
+            md_ext.append(FontAwesomeExtension())
+    else:
+        peli.settings['MD_EXTENSIONS'] = [FontAwesomeExtension()]
+    peli.settings['THEME_STATIC_PATHS'].append('/Users/bmcorser/work/pelicanfly/pelicanfly/static/')
 
 def register():
     signals.initialized.connect(pelicanfly)
-    signals.generator_init.connect(gen_init)
