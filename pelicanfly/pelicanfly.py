a83f066e62530b5c9201093c21ccafcac7a76b58
bmcorser <benmarshallcorser@gmail.com>
2013-08-21 18:30:23 +0100

    Fix asset copying ahead of patch

--- pelicanfly/pelicanfly.py
+++ pelicanfly/pelicanfly.py
@@ -1,10 +1,9 @@
 import os
-import shutil
 from pelican import signals
 from fontawesome_markdown import FontAwesomeExtension
 
 
-def add_markdown_plugin(peli):
+def add_md_ext_and_static(peli):
     md_ext = peli.settings.get('MD_EXTENSIONS')
     cls = FontAwesomeExtension
     inst = cls()
@@ -13,28 +12,15 @@ def add_markdown_plugin(peli):
     elif not any([isinstance(ext, cls) for ext in md_ext]):
         md_ext.append(inst)
         peli.settings['MD_EXTENSIONS'] = md_ext
+    pelifly_static = os.path.join(os.path.split(__file__)[0], 'static')
+    peli.settings['THEME_STATIC_PATHS'].append(pelifly_static)
 
 
 def publish_fontawesome_assets(peli):
-    for ass in ['font', 'css']:
-        input_dir = os.path.join(os.path.split(__file__)[0], 'static', ass)
-        output_dir = os.path.join(peli.output_path, 'theme', ass)
-        try:
-            os.makedirs(output_dir)
-        except OSError as exc:
-            if exc.errno == 17:
-                pass
-        for name in os.listdir(input_dir):
-            input_file = os.path.join(input_dir, name)
-            output_file = os.path.join(output_dir, name)
-            shutil.copy(input_file, output_file)
-
-    css_file = os.path.join(
-                            peli.output_path,
+    css_file = os.path.join(peli.output_path,
                             'theme',
                             'css',
                             peli.settings['CSS_FILE'])
-
     with open(css_file, "r+") as f:
         s = f.read()
         f.seek(0)
@@ -42,5 +28,5 @@ def publish_fontawesome_assets(peli):
 
 
 def register():
-    signals.initialized.connect(add_markdown_plugin)
+    signals.initialized.connect(add_md_ext_and_static)
     signals.finalized.connect(publish_fontawesome_assets)
